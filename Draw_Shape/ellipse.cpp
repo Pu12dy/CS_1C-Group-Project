a41123c099d976776a99a36647df3e7ad414f796
Wadser12346 <lam.ho155@gmail.com>
2019-12-05 17:13:38 -0800

    overrite these
    when possible

--- Draw_Shape/ellipse.cpp
+++ Draw_Shape/ellipse.cpp
@@ -1,6 +1,38 @@
-//#include "ellipse.h"
+#include "ellipse.h"
 
-//ellipse::ellipse()
-//{
+Ellipse::Ellipse() : Ellipse(131,100,100,200,30)
+{
 
-//}
+}
+
+Ellipse::Ellipse(int shapeID,int x1, int y1, int a, int b)
+{
+    setShapeID(shapeID);
+    this->x1 = x1;
+    this->y1 = y1;
+    this->a = a;
+    this->b = b;
+}
+Ellipse::Ellipse(int shapeId, int x1, int y1, int a, int b,
+        std::string penColor, int penWidth, std::string penStyle, std::string penCap, std::string penJoin) : Ellipse(shapeId,x1,y1,a,b)
+{
+    setPenColor(penColor);
+    setPenWidth(penWidth);
+    setPenStyle(penStyle);
+    setPenCapStyle(penCap);
+    setPenJoinStyle(penJoin);
+}
+
+void Ellipse::draw(QPaintDevice *toDraw)
+{
+    getQPainter().begin(toDraw);
+    getQPainter().setPen(getQPen());
+    getQPainter().setBrush(getQBrush());
+    getQPainter().drawEllipse(QRect(x1,y1,a,b));
+    getQPainter().end();
+}
+
+double Ellipse::area()
+{
+    return PI * a * b;
+}
--- Draw_Shape/ellipse.h
+++ Draw_Shape/ellipse.h
@@ -1,23 +1,31 @@
-//#ifndef ELLIPSE_h
-//#define ELLIPSE_h
+#ifndef ELLIPSE_h
+#define ELLIPSE_h
 
-//class Ellipse : public shape
-//{
-//private:
-//    int x1; // point x for center of ellipse
-//    int y1; // point y for center of ellipse
-//    int a;  // semi major axis (width/radius)
-//    int b;  // semi minor axis (height/radius)
+#include "shape.h"
+#include <QPoint>
+#include <string>
 
-//public:
-//    Ellipse();  // Default Constructor
-//    Ellipse(int shapeId, int x1, int y1, int a, int b); // Overload Constructor
-//    ~Ellipse(); // Destructor
 
-//    void draw(); // Draws the shape
-//    void move(int x, int y); // Moves the center of the shape
-//    int perimeter(); // Calculates the perimeters
-//    double area();	 // Calculates the area (PI * a * b = area)
-//};
+class Ellipse : public shape
+{
+private:
+    int x1; // point x for center of ellipse
+    int y1; // point y for center of ellipse
+    int a;  // semi major axis (width/radius)
+    int b;  // semi minor axis (height/radius)
+    int shapeID;
+    const double PI = 3.1415;
 
-//#endif /* ELLIPSE_h */
+public:
+    Ellipse();  // Default Constructor
+    Ellipse(int shapeId, int x1, int y1, int a, int b); // Overload Constructor
+    Ellipse(int shapeId, int x1, int y1, int a, int b, std::string penColor, int penWidth, std::string penStyle, std::string penCap, std::string penJoin);
+   // ~Ellipse(); // Destructor
+
+    void draw(QPaintDevice *toDraw); // Draws the shape
+    //void move(int x, int y); // Moves the center of the shape
+    //int perimeter(); // Calculates the perimeters
+    double area();	 // Calculates the area (PI * a * b = area)
+};
+
+#endif /* ELLIPSE_h */
