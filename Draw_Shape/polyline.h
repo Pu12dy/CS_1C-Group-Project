59643150dee427f0b7d08791ae273495ce867e1e
Purdy <pu12dy@yahoo.com>
2019-12-05 19:39:13 -0800

    Added Polygon and Changes made to Poly line

--- Draw_Shape/polyline.h
+++ Draw_Shape/polyline.h
@@ -4,29 +4,20 @@
 #include "shape.h"
 #include "line.h"
 #include <QPoint>
+#include "vector.h"
 
 class polyline : public shape
 {
 public:
     polyline();
-    polyline(int shapeID, int x1, int y1, int x2, int y2,
-             int x3, int y3);
-
+    polyline(int shapeID, vector<QPoint> p1);
     void draw(QPaintDevice *toDraw);
     //    void move();// Moves the center of the shape
     //    int perimeter();// Calculates the perimeters
     //    double area();// Calculates the area
 
 private:
-    int shapeID;
-    int x1;
-    int y1;
-    int x2;
-    int y2;
-    int x3;
-    int y3;
-    line *firstHalf;
-    line *secondHalf;
+    vector<line*> lines;
 };
 
 #endif // POLYLINE_H
