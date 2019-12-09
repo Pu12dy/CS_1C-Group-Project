275daa60e4123bba26e4dc673f8494b66d0d8b9e
jwidjaja0 <57346642+jwidjaja0@users.noreply.github.com>
2019-12-08 17:09:20 -0800

    Changed Vector and Polygon

--- Draw_Shape/polygon.cpp
+++ Draw_Shape/polygon.cpp
@@ -4,49 +4,34 @@
 
 polygon::polygon()
 {
-    setShapeID(117);
-    // Tester QPoints for polyline
-    vector<QPoint> p1;
+    setShapeID(118);
     QPoint point;
 
     point.setX(150);
     point.setY(250);
-    p1.push_back(point);
+    points.push_back(point);
 
     point.setX(200);
     point.setY(300);
-    p1.push_back(point);
+    points.push_back(point);
 
     point.setX(300);
     point.setY(275);
-    p1.push_back(point);
+    points.push_back(point);
+
 
     point.setX(200);
     point.setY(150);
-    p1.push_back(point);
-
-    for (int i = 0; i < p1.size() - 1; ++i)
-    {
-        line *temp = new line(117, p1[i], p1[i + 1]);
-        lines.push_back(temp);
-    }
-    line *temp = new line(117, p1[0], p1[p1.size() - 1]);
-    lines.push_back(temp);
+    points.push_back(point);
 }
 
-polygon::polygon(int shapeID, vector<QPoint> p1)
+polygon::polygon(int shapeID, Vector<QPoint> p1)
 {
-    setShapeID(shapeID);
-    for (int i = 0; i < p1.size() - 1; ++i)
-    {
-        line *temp = new line(shapeID, p1[i], p1[i + 1]);
-        lines.push_back(temp);
-    }
-    line *temp = new line(117, p1[0], p1[p1.size() - 1]);
-    lines.push_back(temp);
+   setShapeID(shapeID);
+   points = p1;
 }
 
-polygon::polygon(int shapeID, vector<QPoint> p1, std::string penColor, int penWidth, std::string penStyle, std::string penCap, std::string penJoin)
+polygon::polygon(int shapeID, Vector<QPoint> p1, std::string penColor, int penWidth, std::string penStyle, std::string penCap, std::string penJoin)
     : polygon(shapeID, p1)
 {
     setPenColor(penColor);
@@ -60,34 +45,46 @@ polygon::~polygon(){}
 
 void polygon::draw(QPaintDevice *toDraw)
 {
-    for (int i = 0; i < lines.size(); ++i)
-    {
-        lines[i]->draw(toDraw);
-    }
+    getQPainter().begin(toDraw);
+    getQPainter().setPen(getQPen());
+    getQPainter().setBrush(getQBrush());
+    getQPainter().drawPolygon(points.getArrayFromVector(), points.size());
+    getQPainter().end();
 }
 
  void polygon::moveShape(int offsetX, int offsetY)
  {
-     for (int i = 0; i < lines.size(); ++i)
+     for(int i = 0; i < points.size(); ++i)
      {
-         lines[i]->moveShape(offsetX, offsetY);
+        points[i].setX(points[i].x()+offsetX);
+        points[i].setY(points[i].y()+offsetY);
      }
  }
 
  void polygon::changeShapeSize(int newSize)
  {
-     for (int i = 0; i < lines.size(); ++i)
-     {
-         lines[i]->changeShapeSize(newSize);
-     }
+//     for (int i = 0; i < lines.size(); ++i)
+//     {
+//         lines[i]->changeShapeSize(newSize);
+//     }
+
  }
 
  double polygon::perimeter() const
  {
      int result = 0;
-     for (int i = 0; i < lines.size(); ++i)
+//     for (int i = 0; i < lines.size(); ++i)
+//     {
+//       result += QLineF(lines[i]->getP1(), lines[i]->getP2()).length();
+//     }
+//     return result;
+     for(int i = 0; i < points.size()-1; ++i)
      {
-       result += QLineF(lines[i]->getP1(), lines[i]->getP2()).length();
+
+//         QPoint p1(points[i]);
+//         QPoint p2(points[i+1]);
+         //result += QLineF(p1,p2).length();
+         result += QLineF(points[i], points[i+1]).length();
      }
      return result;
  }
