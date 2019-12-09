ea2541a26f679fa6fe82cfa610262523e73d9f33
Purdy <pu12dy@yahoo.com>
2019-12-08 17:10:18 -0800

    New polyline

--- Draw_Shape/polyline.cpp
+++ Draw_Shape/polyline.cpp
@@ -2,97 +2,95 @@
 
 polyline::polyline()
 {
-    setShapeID(116);
-    // Tester QPoints for polyline
-    vector<QPoint> p1;
-    QPoint point;
+	setShapeID(116);
+	QPoint point;
 
-    point.setX(50);
-    point.setY(150);
-    p1.push_back(point);
+	point.setX(50);
+	point.setY(150);
+	points.push_back(point);
 
-    point.setX(100);
-    point.setY(250);
-    p1.push_back(point);
+	point.setX(100);
+	point.setY(250);
+	points.push_back(point);
 
-    point.setX(150);
-    point.setY(150);
-    p1.push_back(point);
+	point.setX(150);
+	point.setY(150);
+	points.push_back(point);
 
-    point.setX(200);
-    point.setY(250);
-    p1.push_back(point);
-
-    for (int i = 0; i < p1.size() - 1; ++i)
-    {
-        line *temp = new line(116, p1[i], p1[i + 1]);
-        lines.push_back(temp);
-    }
+	point.setX(200);
+	point.setY(250);
+	points.push_back(point);
 }
 
-polyline::polyline(int shapeID, vector<QPoint> p1)
+polyline::polyline(int shapeID, Vector<QPoint> p1)
 {
-    setShapeID(shapeID);
-    for (int i = 0; i < p1.size() - 1; ++i)
-    {
-        line *temp = new line(shapeID, p1[i], p1[i + 1]);
-        lines.push_back(temp);
-    }
+	setShapeID(shapeID);
+	points = p1;
+	//    for (int i = 0; i < p1.size() - 1; ++i)
+	//    {
+	//        line *temp = new line(shapeID, p1[i], p1[i + 1]);
+	//        lines.push_back(temp);
+	//    }
 }
 
-polyline::polyline(int shapeID, vector<QPoint> p1, std::string penColor, int penWidth, std::string penStyle, std::string penCap, std::string penJoin)
-    : polyline(shapeID, p1)
+polyline::polyline(int shapeID, Vector<QPoint> p1, std::string penColor, int penWidth, std::string penStyle, std::string penCap, std::string penJoin)
+	: polyline(shapeID, p1)
 {
-    setPenColor(penColor);
-    setPenWidth(penWidth);
-    setPenStyle(penStyle);
-    setPenCapStyle(penCap);
-    setPenJoinStyle(penJoin);
+	setPenColor(penColor);
+	setPenWidth(penWidth);
+	setPenStyle(penStyle);
+	setPenCapStyle(penCap);
+	setPenJoinStyle(penJoin);
 }
 
-polyline::~polyline(){}
+polyline::~polyline() {}
 
 void polyline::draw(QPaintDevice *toDraw)
 {
-    for (int i = 0; i < lines.size(); ++i)
-    {
-        lines[i]->draw(toDraw);
-    }
+
+	getQPainter().begin(toDraw);
+	getQPainter().setPen(getQPen());
+	getQPainter().setBrush(getQBrush());
+	getQPainter().drawPolyline(points.getArrayFromVector(), points.size());
+	getQPainter().end();
+
+	//    for (int i = 0; i < lines.size(); ++i)
+	//    {
+	//        lines[i]->draw(toDraw);
+	//    }
 }
 
 
 void polyline::moveShape(int offsetX, int offsetY)
 {
-    for (int i = 0; i < lines.size(); ++i)
-    {
-        lines[i]->moveShape(offsetX, offsetY);
-    }
-}
-void polyline::changeShapeSize(int newSize)
-{
 
+	for (int i = 0; i < points.size(); ++i)
+	{
+		points[i].setX(points[i].x() + offsetX);
+		points[i].setY(points[i].y() + offsetY);
+	}
 }
 
 
 void polyline::changeShapeSize(int newSize)
 {
-    for (int i = 0; i < lines.size(); ++i)
-    {
-        lines[i]->changeShapeSize(newSize);
-    }
+	//    for (int i = 0; i < lines.size(); ++i)
+	//    {
+	//        lines[i]->changeShapeSize(newSize);
+	//    }
 }
 
 double polyline::perimeter() const
 {
-    return 0;
+	return 0;
 }
 
 double polyline::area() const
 {
-    return 0;
+	return 0;
 }
 
 std::string polyline::getShapeType() const
 {
-    return "Polyline";
+	return "Polyline";
 }
--- Draw_Shape/polyline.h
+++ Draw_Shape/polyline.h
@@ -9,19 +9,20 @@
 class polyline : public shape
 {
 public:
-    polyline();
-    polyline(int shapeID, vector<QPoint> p1);
-    polyline(int shapeID, vector<QPoint> p1, std::string penColor, int penWidth, std::string penStyle, std::string penCap, std::string penJoin);
-    virtual ~polyline();
-    void draw(QPaintDevice *toDraw) override;
-    void moveShape(int offsetX, int offsetY) override;
-    void changeShapeSize(int newSize) override;
-    double perimeter() const; // Calculates the perimeters
-    double area() const;// Calculates the area
-    std::string getShapeType() const override;
+	polyline();
+	polyline(int shapeID, Vector<QPoint> p1);
+	polyline(int shapeID, Vector<QPoint> p1, std::string penColor, int penWidth, std::string penStyle, std::string penCap, std::string penJoin);
+	virtual ~polyline();
+	void draw(QPaintDevice *toDraw) override;
+	void moveShape(int offsetX, int offsetY) override;
+	void changeShapeSize(int newSize) override;
+	double perimeter() const override; // Calculates the perimeters
+	double area() const override;// Calculates the area
+	std::string getShapeType() const override;
 
 private:
-    vector<line*> lines;
+	// vector<line*> lines;
+	Vector<QPoint> points;
 };
 
 #endif // POLYLINE_H
